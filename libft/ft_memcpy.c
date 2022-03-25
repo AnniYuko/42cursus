466adc42341907d86069832c5cdfccb17a227c48
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-03-25 13:33:26 +0100

    tested

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,11 +6,23 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 12:34:16 by akroll            #+#    #+#             */
-/*   Updated: 2022/03/23 14:50:44 by akroll           ###   ########.fr       */
+/*   Updated: 2022/03/25 13:28:25 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void *ft_memcpy(void *dest, const void *src, unsigned int n)
 {
+	const unsigned char *p_src;
+	unsigned char *p_dest;
+	unsigned int i;
+
+	p_dest = dest;
+	p_src = src;
+	i = 0;
+	while (p_src[i] && i < n)
+	{
+		p_dest[i] = p_src[i];
+		i++;
+	}
 	return (dest);
 }
--- tests/memcpy_test.c
+++ tests/memcpy_test.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 14:45:52 by akroll            #+#    #+#             */
-/*   Updated: 2022/03/23 14:51:45 by akroll           ###   ########.fr       */
+/*   Updated: 2022/03/25 13:32:29 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int main()
 	char str[] = "happy birthday";
 	char *p;
 
-	printf("before: %s", str);
+	printf("before: %s\n", str);
 
-	p = ft_memcpy(str, str + 7, 8);
+	p = ft_memcpy(str, str + 6, 5);
 	printf("after memcpy: %s\n", p);
 	return 0;
 }
