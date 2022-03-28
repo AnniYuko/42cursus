eaca28da84f3574930cefc503b06b57ad9c5905a
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-03-28 10:36:11 +0200

    size_t

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 10:50:31 by akroll            #+#    #+#             */
-/*   Updated: 2022/03/24 13:17:42 by akroll           ###   ########.fr       */
+/*   Updated: 2022/03/28 10:35:17 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 //locate byte c in byte string s within the first n bytes
 
-void *ft_memchr(const void *s, int c, unsigned int n)
+void *ft_memchr(const void *s, int c, size_t n)
 {
 	const unsigned char *p;
 
--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,15 +6,17 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 12:34:16 by akroll            #+#    #+#             */
-/*   Updated: 2022/03/25 13:28:25 by akroll           ###   ########.fr       */
+/*   Updated: 2022/03/28 10:17:10 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_memcpy(void *dest, const void *src, unsigned int n)
+#include "libft.h"
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
 {
 	const unsigned char *p_src;
 	unsigned char *p_dest;
-	unsigned int i;
+	size_t i;
 
 	p_dest = dest;
 	p_src = src;
