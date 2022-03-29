8a2f8f50036afe861ceb6cdb4b9377cd67212f20
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-03-29 09:54:58 +0200

    got rid of forbidden VLA, copy from end to start in certain case

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 14:11:30 by akroll            #+#    #+#             */
-/*   Updated: 2022/03/25 14:41:40 by akroll           ###   ########.fr       */
+/*   Updated: 2022/03/28 16:37:40 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,24 @@
 /*
 	- source and destination are allowed to overlap (uses buffer)
 	- doesn't check for '\0' in src, always copies len amount of bytes to dst
+	here: if src > dst, start copying from the end back to the start
 */
 
-void *ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	const unsigned char *p_src;
-	unsigned char *p_dest;
-	unsigned char buffer[len];
-	size_t i;
+	const unsigned char	*p_src;
+	unsigned char		*p_dest;
 
 	p_dest = dst;
 	p_src = src;
-	i = 0;
-	ft_memcpy(buffer, p_src, len);
-	while (i < len)
+	if (p_dest > p_src)
 	{
-		p_dest[i] = buffer[i];
-		i++;
+		while (len-- > 0)
+		{
+			p_dest[len] = p_src[len];
+		}
 	}
+	else
+		ft_memcpy(p_dest, p_src, len);
 	return (dst);
 }
