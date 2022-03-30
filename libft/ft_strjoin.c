eff5db41fe2de14803027dedc2fce7d5fa59d2f4
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-03-30 17:01:22 +0200

    protected

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,36 +6,36 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 10:27:15 by akroll            #+#    #+#             */
-/*   Updated: 2022/03/30 16:57:47 by akroll           ###   ########.fr       */
+/*   Updated: 2022/03/30 17:00:17 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    char *concat_str;
-    int i;
-    int j;
+	char *concat_str;
+	int i;
+	int j;
 
-    i = 0;
-    j = 0;
-    if (s1 == NULL || s2 == NULL)
-        return (NULL);
-    concat_str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
-    if (!concat_str)
-        return(NULL);
-    while (s1[i] != '\0')
-    {
-        concat_str[i] = s1[i];
-        i++;
-    }
-    while (s2[j] != '\0')
-    {
-        concat_str[i] = s2[j];
-        i++;
-        j++;
-    }
-    concat_str[i] = '\0';
-    return(concat_str);
+	i = 0;
+	j = 0;
+	if (s1 == NULL || s2 == NULL)
+	return (NULL);
+	concat_str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!concat_str)
+		return(NULL);
+	while (s1[i] != '\0')
+	{
+		concat_str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		concat_str[i] = s2[j];
+		i++;
+		j++;
+	}
+	concat_str[i] = '\0';
+	return(concat_str);
 }
