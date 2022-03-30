c1e378de41a1244b000710991ceddaa3214443fb
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-03-30 14:48:39 +0200

    doesn't crash if input is NULL p*

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 12:34:16 by akroll            #+#    #+#             */
-/*   Updated: 2022/03/28 14:04:59 by akroll           ###   ########.fr       */
+/*   Updated: 2022/03/30 14:42:39 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	p_dest = (unsigned char *)dest;
 	p_src = (const unsigned char *)src;
 	i = 0;
-	while (i < n)
+	while (i < n && (p_dest != NULL && p_src != NULL))
 	{
 		p_dest[i] = p_src[i];
 		i++;
