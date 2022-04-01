b0b22df1825c1cc0667d87290afcedc16e87cc95
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-04-01 09:24:23 +0200

    fixed type conversion and recursion

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 10:47:52 by akroll            #+#    #+#             */
-/*   Updated: 2022/03/29 12:21:46 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/01 09:23:33 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,16 @@ void	ft_putnbr_fd(int n, int fd)
 		if (n == -2147483648)
 			write(fd, "-2147483648", 11);
 		else
+		{
 			ft_putchar_fd('-', fd);
+			ft_putnbr_fd(-n, fd);
+		}
 	}
-	if (n < 10)
-		ft_putchar_fd((unsigned char)n, fd);
+	else if (n < 10)
+		ft_putchar_fd(n + '0', fd);
 	else
 	{
 		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		ft_putchar_fd((n % 10) + '0', fd);
 	}
 }
