6e7e922c221ad74e5bd4d85c31e9515363757c01
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-04-02 17:31:43 +0200

    norminette OK

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 10:27:15 by akroll            #+#    #+#             */
-/*   Updated: 2022/03/30 17:00:17 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/02 17:31:14 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char *concat_str;
-	int i;
-	int j;
+	char	*concat_str;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
 	if (s1 == NULL || s2 == NULL)
-	return (NULL);
+		return (NULL);
 	concat_str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
 	if (!concat_str)
-		return(NULL);
+		return (NULL);
 	while (s1[i] != '\0')
 	{
 		concat_str[i] = s1[i];
@@ -37,5 +37,5 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		j++;
 	}
 	concat_str[i] = '\0';
-	return(concat_str);
+	return (concat_str);
 }
