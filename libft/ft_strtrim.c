79085ed3770d416be12bc80f5fd2c67cc8d35265
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-04-07 20:24:27 +0200

    protected, works with empty string

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,11 +6,29 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 13:02:22 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/07 15:47:48 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/07 20:13:30 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+// unsigned int	skip_charset(char const *s1, char const *set, int count)
+// {
+// 	unsigned int	i;
+// 	unsigned int	direction;
+
+// 	i = 0;
+// 	if (count > 0)
+// 		direction = 1;
+// 	while (ft_strchr(set, s1[i]) != NULL)
+// 	{
+// 		if (direction == 1)
+// 			i++;
+// 		else
+// 			i--;
+// 	}
+// 	return (i);
+// }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -21,12 +39,14 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	i = 0;
 	count = 0;
+	if (s1 == NULL || set == NULL)
+		return (NULL);
 	len = ft_strlen(s1);
 	while (ft_strchr(set, s1[i]) != NULL)
 	{
 		i++;
 	}
-	while (ft_strchr(set, s1[len]) != NULL)
+	while (ft_strchr(set, s1[len]) != NULL && len > 0)
 	{
 		len--;
 	}
