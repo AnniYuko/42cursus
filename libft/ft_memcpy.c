4211e4a66fd735cdeb5d3cba51d3e44befd50d54
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-04-08 17:08:15 +0200

    protect against both input parameters are NULL p*

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 12:34:16 by akroll            #+#    #+#             */
-/*   Updated: 2022/03/30 14:42:39 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/08 16:21:53 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*p_dest;
 	size_t				i;
 
+	if (dest == NULL && src == NULL)
+		return (NULL);
 	p_dest = (unsigned char *)dest;
 	p_src = (const unsigned char *)src;
 	i = 0;
-	while (i < n && (p_dest != NULL && p_src != NULL))
+	while (i < n)
 	{
 		p_dest[i] = p_src[i];
 		i++;
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,14 +6,14 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 14:11:30 by akroll            #+#    #+#             */
-/*   Updated: 2022/03/28 16:37:40 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/08 16:20:25 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-	- source and destination are allowed to overlap (uses buffer)
+	- source and destination are allowed to overlap
 	- doesn't check for '\0' in src, always copies len amount of bytes to dst
 	here: if src > dst, start copying from the end back to the start
 */
@@ -25,6 +25,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	p_dest = dst;
 	p_src = src;
+	if (dst == NULL && src == NULL)
+		return (NULL);
 	if (p_dest > p_src)
 	{
 		while (len-- > 0)
