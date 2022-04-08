551d4301a24fddb9b8e2bb38a8bdc360c55a932f
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-04-08 15:32:04 +0200

    works with non-ascii characters, not protected

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 12:34:10 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/01 09:05:51 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/08 15:30:05 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (n == 0 || s1 == NULL || s2 == NULL)
-		return (0);
-	while (i < n && (s1[i] != '\0' && s2[i] != '\0'))
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
 		if (s1[i] != s2[i])
-			break ;
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	if (s1[i] != s2[i])
-		return (s1[i] - s2[i]);
 	return (0);
 }
 
@@ -35,6 +31,5 @@ ft_strncmp:    [OK] [OK] [OK] [OK] [OK] [OK] [FAILED] [OK] [OK] [FAILED] [FAILED
 [fail]: your strncmp does not cast in unsigned char the diff
 [fail]: your strncmp does not work with non ascii chars
 [fail]: your strncmp does not work when s1 is contained in s2
-[no crash]: your strncmp does not segfault when null parameter is sent
 [no crash]: your strncmp does not segfault when null parameter is sent
 */
