a5eb970b8afb343370c7580160ef08607679b430
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-04-11 11:44:16 +0200

    deleted comment, norminette

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 12:34:10 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/08 15:30:05 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/11 11:43:36 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,3 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return (0);
 }
-
-/*
-ft_strncmp:    [OK] [OK] [OK] [OK] [OK] [OK] [FAILED] [OK] [OK] [FAILED] [FAILED] [OK] [NO CRASH] [NO CRASH]
-[fail]: your strncmp does not cast in unsigned char the diff
-[fail]: your strncmp does not work with non ascii chars
-[fail]: your strncmp does not work when s1 is contained in s2
-[no crash]: your strncmp does not segfault when null parameter is sent
-*/
