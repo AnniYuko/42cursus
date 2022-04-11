53acf0d446e27c5dfea997ce4d15f6e04ca4c8d4
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-04-11 11:40:51 +0200

    don't ignore additional +, - in the beginning

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,14 +6,14 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 16:18:06 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/08 17:16:12 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/11 11:35:20 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	sign;
+	int				i;
+	int				sign;
 	long long int	nb;
 
 	nb = 0;
@@ -24,10 +24,10 @@ int	ft_atoi(const char *str)
 	{
 		i++;
 	}
-	while (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
+	if (str[i] == '-')
 			sign = -1;
+	if (str[i] == '+' || str[i] == '-')
+	{
 		i++;
 	}
 	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
