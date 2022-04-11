e0e8118ba9682034a8af88ad7a87200717efd76e
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-04-11 15:32:44 +0200

    don't alter string if destsize < destlen

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 15:15:44 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/08 16:52:54 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/11 15:30:39 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,22 @@ size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
 	size_t	i;
 	size_t	destlen;
 	size_t	srclen;
+	size_t	start;
 
 	i = 0;
 	destlen = ft_strlen(dest);
 	srclen = ft_strlen(src);
-	if (destsize == 0)
-		return (destsize + srclen);
-	while (src[i] != '\0' && (destlen + i) < destsize - 1)
-	{
-		dest[destlen + i] = src[i];
-		i++;
-	}
-	dest[destlen + i] = '\0';
 	if (destsize < destlen)
-		return (destsize + srclen);
-	return (destlen + srclen);
+		start = destsize;
+	else
+	{
+		start = destlen;
+		while (src[i] != '\0' && (start + i) < (destsize - 1))
+		{
+			dest[start + i] = src[i];
+			i++;
+		}
+		dest[start + i] = '\0';
+	}
+	return (start + srclen);
 }
--- tests/strlcat_test.c
+++ tests/strlcat_test.c
@@ -6,23 +6,25 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 15:14:52 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/08 15:15:26 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/11 15:30:24 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include <string.h>
 
-int	main()
+int main()
 {
-	int	size = 0;
-	char dest[20] = "bla ";
-	char dest_[20] = "bla ";
+	char *dest;
+	char *dest_;
+	const char *src;
 
-	printf("strlcat:    %lu\n", strlcat(dest, "concat this", size));
-	printf("ft_strlcat: %lu\n", ft_strlcat(dest_, "concat this", size));
+	dest  = "rrrrrrrrrrrrrrr";
+	dest_ = "rrrrrrrrrrrrrrr";
+	src = "lorem ipsum dolor sit";
+	printf("ft_strlcat\n\t%zu\n", ft_strlcat(dest_, src, 0));
+	printf("\t%s\n", dest_);
 
-	printf("\nresulting strings:\n");
-	printf("\tstrlcat:    %s\n", dest);
-	printf("\tft_strlcat: %s\n", dest_);
+	printf("strlcat\n\t%lu\n", strlcat(dest, src, 0));
+	printf("\t%s\n", dest);
 }
