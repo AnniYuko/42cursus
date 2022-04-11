e006a39434f996eaf73ea2b0a0b3f70174deed61
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-04-11 17:20:51 +0200

    norminette

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 17:38:34 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/08 16:55:39 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/11 15:39:14 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,28 +59,28 @@ static unsigned int	count_strings(char const *s, char c)
 char	**ft_split(char const *s, char c)
 {
 	char			**split_array;
-	unsigned int	num_strings;
 	unsigned int	string_len;
 	unsigned int	i;
 	unsigned int	k;
 
-	if (s == NULL)		//protect
-		return (NULL);
-	num_strings = count_strings(s, c);
-	if ((split_array = malloc((num_strings + 1) * sizeof(char *))) == NULL)	//malloc and protect
-		return (NULL);
 	i = 0;
 	k = 0;
-	while (k < num_strings)
+	if (s == NULL)
+		return (NULL);
+	split_array = malloc((count_strings(s, c) + 1) * sizeof(char *));
+	if (split_array == NULL)
+		return (NULL);
+	split_array[count_strings(s, c)] = NULL;
+	while (split_array[k] != NULL)
 	{
 		i += skip_delimiter_characters(&s[i], c);
-		i += (string_len = iterate_word(&s[i], c));
+		string_len = iterate_word(&s[i], c);
+		i += string_len;
 		split_array[k] = malloc((string_len + 1) * sizeof(char));
 		if (&split_array[k] == NULL)
 			return (NULL);
 		ft_strlcpy(split_array[k], &s[i] - string_len, string_len + 1);
 		k++;
 	}
-	split_array[k] = NULL;		// The array must end with a NULL pointer.
 	return (split_array);
 }
