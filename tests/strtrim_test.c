4d8a7f0bdd0900f48da717d6b5c32381be8e8d17
Annika Kroll <akroll@1-D-2.42heilbronn.de>
2022-04-15 13:31:35 +0200

    new test cases

--- tests/strtrim_test.c
+++ tests/strtrim_test.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 15:48:32 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/12 15:55:24 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/14 14:23:45 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 int	main()
 {
-	char str[] = "ete.....petete";
-	char charset[] = "tep";
 	char *trimmed_str;
 
-	trimmed_str = ft_strtrim(str, charset);
+	trimmed_str = ft_strtrim("", "abd");
 	printf("trimmed string: %s\n", trimmed_str);
 	printf("length: %zu\n", ft_strlen(trimmed_str));
 
