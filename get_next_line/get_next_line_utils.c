d63ff4fc699cdfc48d33227fc086bd121386d994
Annika Kroll <akroll@1-B-2.42heilbronn.de>
2022-04-26 14:57:33 +0200

    issues comment

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 15:48:36 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/25 17:50:53 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/26 14:57:14 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,13 @@ char *get_next_line(int fd)
 {
 
 }
+
+/* issues:
+
+- static char *content_after_newline
+- always copies entire buffer (should stop after newline)
+- what if entire file is empty? return empty string?
+- what happens if read returns 0? quit while loop and return (newstring)
+- read has an error -> return NULL
+
+*/
