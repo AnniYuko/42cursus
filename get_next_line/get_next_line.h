826123c2c38eca9fe96082908a73d8c53bf400d9
Annika Kroll <akroll@1-B-2.42heilbronn.de>
2022-04-27 20:09:32 +0200

    add libraries

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,14 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 15:46:52 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/25 15:49:52 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/27 15:56:10 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
 
 char *get_next_line(int fd);
 
