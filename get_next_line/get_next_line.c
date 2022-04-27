ea3439c8065d0cd0cc37c1859af8a0c61d2a57d9
Annika Kroll <akroll@1-B-2.42heilbronn.de>
2022-04-27 19:06:36 +0200

    adds buffer chunk to string, calloc

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 15:47:32 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/27 16:27:26 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/27 19:04:52 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,53 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-// size_t	ft_strlen(const char *s)
-// {
-// 	size_t	len;
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
 
-// 	len = 0;
-// 	while (s[len] != '\0')
-// 		len++;
-// 	return (len);
-// }
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	unsigned char	*mem;
+	unsigned int	i;
+	size_t			n_bytes;
+
+	i = 0;
+	n_bytes = count * size;
+	mem = malloc(n_bytes);
+	if (mem == NULL)
+		return (NULL);
+	while (i < n_bytes)
+	{
+		mem[i] = '\0';
+		i++;
+	}
+	return (mem);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dest;
+	int		i;
+	int		length;
+
+	i = 0;
+	length = ft_strlen(s1);
+	dest = malloc(sizeof(char) * (length + 1));
+	if (dest == NULL)
+		return (NULL);
+	while (i < length)
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	return (dest);
+}
 
 int	no_newline_found(const char *buffer)
 {
@@ -38,15 +76,31 @@ int	no_newline_found(const char *buffer)
 	return (1);		/* true */
 }
 
-// char *add_buffer_chunk_to_string(const char *buffer, const char *string_out)
-// {
-// 	char *new_joined_str;
+char *add_buffer_chunk_to_string(const char *buffer, char *string)
+{
+	char	*new_joined_str;
+	int		i;
+	int		j;
 
-// 	new_joined_str = malloc(ft_strlen(buffer) + ft_strlen(string_out) + 1);
-
-// 	free(string_out);
-// 	return (new_joined_str);
-// }
+	i = 0;
+	j = 0;
+	new_joined_str = malloc(ft_strlen(buffer) + ft_strlen(string) + 1);
+	if (new_joined_str == NULL)
+		return (NULL);
+	while (string[i] != '\0')
+	{
+		new_joined_str[i] = string[i];
+		i++;
+	}
+	while (buffer[j] != '\0')
+	{
+		new_joined_str[i + j] = buffer[j];
+		j++;
+	}
+	new_joined_str[i + j] = '\0';
+	free(string);
+	return (new_joined_str);
+}
 
 char *get_next_line(int fd)
 {
@@ -55,19 +109,19 @@ char *get_next_line(int fd)
 	int		b;
 
 	b = 0;
-	string_out = "\0";		/* allowed to use stack? */
-	buffer = malloc(BUFFER_SIZE + 1);
+	string_out = ft_strdup("\0");
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (buffer == NULL)
 		return (NULL);
 
 	printf("buffer content\n");
 	while (no_newline_found(buffer))
 	{
-		b++;
 		/* get return value = */ read(fd, buffer, BUFFER_SIZE);
 		buffer[BUFFER_SIZE] = '\0';
 		printf("\t%d: %s\n", b, buffer);
-		//add_buffer_chunk_to_string(buffer, string_out);
+		string_out = add_buffer_chunk_to_string(buffer, string_out);
+		b++;
 	}
 	// newline found:
 
@@ -80,6 +134,7 @@ int	main()
 	int	fd;
 
 	fd = open("test.txt", O_RDONLY);
+	printf("output: %s\n", get_next_line(fd));
 	printf("output: %s\n", get_next_line(fd));
 }
 
