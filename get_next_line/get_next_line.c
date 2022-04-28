7a5d5f8c92d0346233e5b12ea2ae4b684b11df33
Annika Kroll <akroll@1-B-2.42heilbronn.de>
2022-04-28 11:19:15 +0200

    copies characters after newline into static char*

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 15:47:32 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/27 20:06:54 by akroll           ###   ########.fr       */
+/*   Updated: 2022/04/28 11:18:32 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,24 @@ size_t	ft_strlen(const char *s)
 	return (len);
 }
 
-void	*ft_calloc(size_t count, size_t size)
-{
-	unsigned char	*mem;
-	size_t			i;
-	size_t			n_bytes;
+// void	*ft_calloc(size_t count, size_t size)
+// {
+// 	unsigned char	*mem;
+// 	size_t			i;
+// 	size_t			n_bytes;
 
-	i = 0;
-	n_bytes = count * size;
-	mem = malloc(n_bytes);
-	if (mem == NULL)
-		return (NULL);
-	while (i < n_bytes)
-	{
-		mem[i] = '\0';
-		i++;
-	}
-	return (mem);
-}
+// 	i = 0;
+// 	n_bytes = count * size;
+// 	mem = malloc(n_bytes);
+// 	if (mem == NULL)
+// 		return (NULL);
+// 	while (i < n_bytes)
+// 	{
+// 		mem[i] = '\0';
+// 		i++;
+// 	}
+// 	return (mem);
+// }
 
 char	*ft_strldup(const char *s1, size_t length)
 {
@@ -78,7 +78,7 @@ int	newline_found(const char *buffer, size_t *bytes_til_newl)
 	return (0);		/* false */
 }
 
-char *add_to_new_string(const char *buffer, char *string)
+char *ft_strljoin(const char *buffer, char *string, size_t length)
 {
 	char	*new_joined_str;
 	int		i;
@@ -86,7 +86,7 @@ char *add_to_new_string(const char *buffer, char *string)
 
 	i = 0;
 	j = 0;
-	new_joined_str = malloc(ft_strlen(buffer) + ft_strlen(string) + 1);
+	new_joined_str = malloc(ft_strlen(buffer) + length + 1);
 	if (new_joined_str == NULL)
 		return (NULL);
 	while (string[i] != '\0')
@@ -94,7 +94,7 @@ char *add_to_new_string(const char *buffer, char *string)
 		new_joined_str[i] = string[i];
 		i++;
 	}
-	while (buffer[j] != '\0')
+	while (buffer[j] != '\0' && j < length)
 	{
 		new_joined_str[i + j] = buffer[j];
 		j++;
@@ -106,29 +106,34 @@ char *add_to_new_string(const char *buffer, char *string)
 
 char *get_next_line(int fd)
 {
-	//static char *str_after_newl;
+	static char *str_after_newl;
 	char		*buffer;
 	char		*string_out;
 	size_t		bytes_til_newl;
-	int			bytes_read;
+	size_t		bytes_read;
 
 	string_out = ft_strldup("\0", 1);
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	buffer = malloc(BUFFER_SIZE + 1);
 	if (buffer == NULL)
 		return (NULL);
 
-	while (bytes_read != 0)
+	// check static char* for text (til a newline)
+
+	while (bytes_read > 0)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		buffer[BUFFER_SIZE] = '\0';
 		printf("\tbuffer: %s\n", buffer);
 		if (newline_found(buffer, &bytes_til_newl))
+		{
+			string_out = ft_strljoin(buffer, string_out, bytes_til_newl + 1);		/* (+ 1) includes newline character */
+			str_after_newl = ft_strldup(&buffer[bytes_til_newl + 1], BUFFER_SIZE - bytes_til_newl);
 			break ;
-		string_out = add_to_new_string(buffer, string_out);
+		}
+		else
+			string_out = ft_strljoin(buffer, string_out, BUFFER_SIZE);
 	}
-	string_out = add_to_new_string(ft_strldup(buffer, bytes_til_newl), string_out);
-	// copy rest into static pointer
-
+	printf("static: %s\n", str_after_newl);
 	return (string_out);
 }
 
@@ -144,9 +149,8 @@ int	main()
 
 /* issues:
 
-- static char *content_after_newline
-- always copies entire buffer (should stop after newline)
 - what if entire file is empty? return empty string?
-- what happens if read returns 0? quit while loop and return (newstring)
 - read has an error -> return NULL
+- leaks
+
 */
