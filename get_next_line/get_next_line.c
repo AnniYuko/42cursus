1d7261688c919e985d2748a8e64805f42c478404
Annika Kroll <akroll@1-B-2.42heilbronn.de>
2022-05-04 16:27:37 +0200

    debug printf for catching leaks

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 15:47:32 by akroll            #+#    #+#             */
-/*   Updated: 2022/05/03 18:35:02 by akroll           ###   ########.fr       */
+/*   Updated: 2022/05/04 16:26:02 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ size_t	get_length(const char *string)
 	len = 0;
 	while (string != NULL && (string[len] != '\0' && string[len] != '\n'))
 		len++;
-	return (len);
+	return (len + 1);	/* (+1) inlcudes '\0' or '\n' */
 }
 
 int	ft_strchr_bool(const char *s, int c)
@@ -63,7 +63,7 @@ int	ft_strchr_bool(const char *s, int c)
 	return (*s == (unsigned char)c);
 }
 
-char *ft_strljoin(const char *added, char *string, size_t length)
+char *ft_strljoin(char *added, char *string, size_t length)
 {
 	char	*new_joined_str;
 	size_t	i;
@@ -81,15 +81,37 @@ char *ft_strljoin(const char *added, char *string, size_t length)
 		new_joined_str[i] = string[i];
 		i++;
 	}
-	while (added[j] != '\0' && j < length)
+	while (/* added != NULL && */ added[j] != '\0' && j < length)
 	{
 		new_joined_str[i + j] = added[j];
 		j++;
 	}
 	new_joined_str[i + j] = '\0';
+	// free(added); //?
 	free(string);
 	string = NULL;
 	return (new_joined_str);
+}
+
+char *shorten_string(char *string, size_t start)
+{
+	size_t	i;
+	char	*shortened_str;
+
+	i = 0;
+	if (start == 0)
+		return (string);
+	shortened_str = malloc(ft_strlen(string + start) + 1);
+	while (string[start + i] != '\0')
+	{
+		shortened_str[i] = string[start + i];
+		i++;
+	}
+	string[start + i] = '\0';
+	// if (string != NULL)
+	// 	free(string);
+	// string = NULL;
+	return (shortened_str);
 }
 
 char *get_next_line(int fd)
@@ -97,18 +119,23 @@ char *get_next_line(int fd)
 	static char *str_after_newl;
 	char		*buffer;
 	char		*string_out;
-	size_t		length;
+	ssize_t		length;
 	ssize_t		bytes_read;
 
 	string_out = ft_strdup("\0");
+	if (!str_after_newl)
+		str_after_newl = ft_strdup("\0");
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (buffer == NULL)
 		return (NULL);
 	bytes_read = 1;
 
-	length = get_length(str_after_newl);								/* til \n or \0 */
-	string_out = ft_strljoin(str_after_newl, string_out, length + 1);	/* (+ 1) includes newline character */
-	str_after_newl += length;	// leaks because of this?
+	length = get_length(str_after_newl);						/* til \n or \0 */
+	if (length > 0)
+		string_out = ft_strljoin(str_after_newl, string_out, length);
+		// printf("\tstring_out 1: %s, %p\n", string_out, string_out);
+		str_after_newl = shorten_string(str_after_newl, length);
+		// printf("\tstr_after_newl 1: %s, %p\n", str_after_newl, str_after_newl);
 	while (!ft_strchr_bool(string_out, '\n') && bytes_read != 0)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
@@ -118,11 +145,14 @@ char *get_next_line(int fd)
 			return (NULL);
 		}
 		buffer[BUFFER_SIZE] = '\0';
-		length = get_length(buffer);								/* til \n or \0 */
-		string_out = ft_strljoin(buffer, string_out, length + 1);	/* (+ 1) includes newline character */
-		printf("\tstored: %s\n", string_out);
-		str_after_newl = ft_strdup(buffer + length + 1);
-	//printf("static: %s\n", str_after_newl);
+		printf("buffer: %s\n", buffer);
+		length = get_length(buffer);							/* til \n or \0 */
+		string_out = ft_strljoin(buffer, string_out, length);
+		// printf("\tstring_out 2: %s, %p\n", string_out, string_out);
+		// printf("\tstatic old: %s, %p\n", str_after_newl, str_after_newl);
+		if (bytes_read > length)
+			str_after_newl = shorten_string(buffer, length);
+		// printf("\tstatic shortened: %s, %p\n", str_after_newl, str_after_newl);
 	}
 	free(buffer);
 	return (string_out);
@@ -131,16 +161,12 @@ char *get_next_line(int fd)
 int	main()
 {
 	int		fd;
-	char	*output;
 
 	fd = open("test.txt", O_RDONLY);
-	output = get_next_line(fd);
 
-	printf("output: %s\n", output);
-	// printf("output: %s\n", get_next_line(fd));
-	// printf("output: %s\n", get_next_line(fd));
-	// printf("output: %s\n", get_next_line(fd));
-	free(output);
+	printf("output: %s\n", get_next_line(fd));
+	printf("output: %s\n", get_next_line(fd));
+	printf("output: %s\n", get_next_line(fd));
 	system("leaks a.out");
 	return (0);
 }
@@ -148,8 +174,7 @@ int	main()
 
 /* issues:
 
-- what if entire file is empty? return empty string?
-- read has an error -> return NULL
+- ? "pointer being freed was not allocated"
 - leaks
 
 */
