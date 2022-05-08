dfe56569234c1f45c65e0094239f5b160a7bef23
AnniYuko <yuko99@outlook.de>
2022-05-08 11:10:59 +0200

    summarized into one function

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,49 +6,35 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 13:02:22 by akroll            #+#    #+#             */
-/*   Updated: 2022/04/14 14:26:29 by akroll           ###   ########.fr       */
+/*   Updated: 2022/05/08 11:06:04 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static unsigned int	skip_charset_from_start(char const *s1, char const *set)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (ft_strchr(set, s1[i]) != NULL && s1[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char			*str_trimmed;
 	unsigned int	i;
-	unsigned int	count;
+	unsigned int	bytes_to_copy;
 	unsigned int	len;
 
 	i = 0;
-	count = 0;
+	bytes_to_copy = 0;
+
 	if (s1 == NULL || set == NULL)
 		return (NULL);
 	len = ft_strlen(s1);
-	if (len == 0)
-		return (ft_strdup(""));
-	i = skip_charset_from_start(s1, set);
+	while (ft_strchr(set, s1[i]) != NULL && s1[i] != '\0')
+		i++;
 	while (ft_strchr(set, s1[len - 1]) != NULL && i < len)
-	{
 		len--;
-	}
-	if (i == len)
+	if (i == len || len == 0)
 		return (ft_strdup(""));
-	count = len - i;
-	str_trimmed = malloc((count + 1) * sizeof(char));
+	bytes_to_copy = len - i;
+	str_trimmed = malloc((bytes_to_copy + 1) * sizeof(char));
 	if (str_trimmed == NULL)
 		return (NULL);
-	ft_strlcpy(str_trimmed, &s1[i], count + 1);
+	ft_strlcpy(str_trimmed, &s1[i], bytes_to_copy + 1);
 	return (str_trimmed);
 }
