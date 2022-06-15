4a746b7f054e3850e2d48552fed294d58c077311
AnniYuko <yuko99@outlook.de>
2022-06-15 14:50:19 +0200

    add prototypes

--- printf/libftprintf.h
+++ printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 16:26:15 by akroll            #+#    #+#             */
-/*   Updated: 2022/05/17 18:35:18 by akroll           ###   ########.fr       */
+/*   Updated: 2022/06/15 14:10:00 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 # define LIBFTPRINTF_H
 # include <stdarg.h>
 # include <stdlib.h>
+# include "libft/libft.h"
 
-int	ft_printf(const char *, ...);
-// void	ft_putnbr_unsigned_fd(unsigned int n, int fd);
+int		ft_printf(const char *, ...);
+int		ft_putstr_fd_count(char *s, int fd, int* count);
+int		ft_putchar_fd_count(char c, int fd, int* count);
+void	ft_putnbr_fd_unsigned(unsigned int n, int fd, int* count);
+void	ft_puthex_fd(unsigned long n, char option, int fd, int* count);
+void	ft_put_next_arg(char option, va_list ap, int* count);
 
 #endif
