346f151d0cbaf2cc4a5994b80138f2aea2a3cc07
AnniYuko <yuko99@outlook.de>
2022-06-15 14:51:10 +0200

    fixed %p and %x%X, added count

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,76 +6,53 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 13:26:55 by akroll            #+#    #+#             */
-/*   Updated: 2022/05/17 18:45:39 by akroll           ###   ########.fr       */
+/*   Updated: 2022/06/15 14:37:05 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-#include "libft/libft.h"
 
-void	ft_putnbr_unsigned_fd(unsigned int n, int fd)
-{
-	if (n < 10)
-		ft_putchar_fd(n + '0', fd);
-	else
-	{
-		ft_putnbr_unsigned_fd(n / 10, fd);
-		ft_putchar_fd((n % 10) + '0', fd);
-	}
-}
 
-void	ft_puthex_fd(unsigned long n, char option, int fd)
-{
-	if (n < 10)
-		ft_putchar_fd(n + '0', fd);
-	else if (n <= 16 && (option == 'x' || option == 'p'))
-		ft_putchar_fd('a' + (n - 10), fd);
-	else if (n <= 16 && option == 'X')
-		ft_putchar_fd('A' + (n - 10), fd);
-	else
-	{
-		ft_puthex_fd(n / 16, option, fd);
-		ft_puthex_fd(n % 16, option, fd);
-	}
-}
-
-void	ft_put_next_arg(char option, va_list ap)
+void	ft_put_next_arg(char option, va_list ap, int* count)
 {
 	if (option == '%')
-		ft_putchar_fd('%', 1);
-	if (option == 'c')
-		ft_putchar_fd((char)va_arg(ap, int), 1);
+		ft_putchar_fd_count('%', 1, count);
+	else if (option == 'c')
+		ft_putchar_fd_count((char)va_arg(ap, int), 1, count);
 	else if (option == 's')
-		ft_putstr_fd(va_arg(ap, char *), 1);
+		ft_putstr_fd_count(va_arg(ap, char *), 1, count);
 	else if (option == 'd' || option == 'i')
-		ft_putnbr_fd(va_arg(ap, int), 1);
+		ft_putstr_fd_count(ft_itoa(va_arg(ap, int)), 1, count);
 	else if (option == 'u')
-		ft_putnbr_unsigned_fd(va_arg(ap, unsigned int), 1);
+		ft_putnbr_fd_unsigned(va_arg(ap, unsigned int), 1, count);
 	else if (option == 'x' || option == 'X' || option == 'p')
 	{
-		// ft_putstr_fd("0x", 1);
-		ft_puthex_fd(va_arg(ap, unsigned long), option, 1);
+		if (option == 'p')
+			ft_putstr_fd_count("0x", 1, count);
+		ft_puthex_fd(va_arg(ap, unsigned long), option, 1, count);
 	}
 }
 
 int	ft_printf(const char *str, ...)
 {
-	int	i;
+	int		i;
+	int		count_printed;
 	va_list	ap;
 
 	i = 0;
+	count_printed = 0;
 	va_start(ap, str);
 	while (str[i] != '\0')
 	{
-		if (str[i] == '%')
+		if (str[i] == '%' && str[i + 1] != '\0')
 		{
 			i++;
-			ft_put_next_arg(str[i], ap);
+			ft_put_next_arg(str[i], ap, &count_printed);
 		}
 		else
-			ft_putchar_fd(str[i], 1);
+			ft_putchar_fd_count(str[i], 1, &count_printed);
 		i++;
 	}
 	va_end(ap);
-	return (0);
+	return (count_printed);
 }
