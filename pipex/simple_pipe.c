3dc3e1292393d013edd851564c52b629edc0ab66
AnniYuko <yuko99@outlook.de>
2022-06-20 13:12:18 +0200

    argc argv

--- pipex/simple_pipe.c
+++ pipex/simple_pipe.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tests.c                                            :+:      :+:    :+:   */
+/*   simple_pipe.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 22:48:24 by akroll            #+#    #+#             */
-/*   Updated: 2022/06/17 02:16:18 by akroll           ###   ########.fr       */
+/*   Updated: 2022/06/20 13:11:55 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	int		fd[2];
-	pid_t	childpid;
+	pid_t	pid;
 	char	message[] = "hi parent";
 	char	readbuffer[30];
 
+	printf("argc: %d\n", argc);
+	(void)argv;
+
 // The pipe() call must be made BEFORE a call to fork(), or the descriptors will not be inherited by the child!
 	pipe(fd);
-	childpid = fork();	// a child process inherits any open file descriptors from the parent
-	if (childpid == -1)
+	pid = fork();	// a child process inherits any open file descriptors from the parent
+	if (pid == -1)
 	{
 		perror("fork");
 		exit(1);
 	}
-	if (childpid == 0)	// child process
+	if (pid == 0)	// child process
 	{
 		close(fd[0]);
 		write(fd[1], message, strlen(message) + 1);
 		exit(0);
 	}
-	else				// parent process
+	else			// parent process, pid == 1
 	{
 		close(fd[1]);
 		read(fd[0], readbuffer, sizeof(readbuffer));
