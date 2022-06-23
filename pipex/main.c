c7cc2059bb0c2e1e65c5a60e7b7d0e7e648631e6
AnniYuko <yuko99@outlook.de>
2022-06-23 09:07:54 +0200

    input file1, output to file

--- pipex/main.c
+++ pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:06:53 by akroll            #+#    #+#             */
-/*   Updated: 2022/06/22 16:49:22 by akroll           ###   ########.fr       */
+/*   Updated: 2022/06/22 17:32:16 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,19 @@ int	main(int argc, char **argv)
 	pid_t	pid2;
 	char	*arguments[] = {"/bin/bash", "-c", argv[2], "-c", argv[3], 0};
 
-	fd_file1 = open(argv[1], O_RDONLY);
-	printf("fd file1: %d\n", fd_file1);
-	fd_file2 = open(argv[4], O_WRONLY | O_CREAT, 00700);
-	printf("fd file2: %d\n", fd_file2);
-
 	if (argc != 5)
 	{
 		printf("Invalid number of arguments\n");
 		return (1);
 	}
+
+	if ((fd_file1 = open(argv[1], O_RDONLY)) == -1)
+		perror(argv[1]);
+	if ((fd_file2 = open(argv[4], O_WRONLY | O_CREAT, 00700)) == -1)
+		perror(argv[4]);
+
+	if (fd_file1 == -1 || fd_file2 == -1)
+		return (1);
 
 // The pipe() call must be made BEFORE a call to fork(), or the descriptors will not be inherited by the child!
 	if (pipe(fd) == -1)
@@ -47,7 +50,11 @@ int	main(int argc, char **argv)
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd_file1);
 		close(fd[1]);
-		execve("/bin/bash", arguments, 0);
+		if (execve("/bin/bash", arguments, 0) == -1)
+		{
+			perror("execve pid");
+			return (1);
+		}
 	}
 	if ((pid2 = fork()) == -1)
 	{
@@ -62,8 +69,11 @@ int	main(int argc, char **argv)
 		dup2(fd_file2, STDOUT_FILENO);
 		close(fd_file2);
 		close(fd[0]);
-		execve("/bin/bash", &arguments[2], 0);
-
+		if (execve("/bin/bash", &arguments[2], 0) == -1)
+		{
+			perror("execve pid2");
+			return (1);
+		}
 		exit(0);
 	}
 	close(fd[0]);
