5608acfd0856494c6aadf076d7e28463bb2e0cc2
AnniYuko <yuko99@outlook.de>
2022-07-04 14:38:10 +0200

    compile with libft

--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -6,18 +6,19 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 12:48:50 by akroll            #+#    #+#             */
-/*   Updated: 2022/06/29 13:27:28 by akroll           ###   ########.fr       */
+/*   Updated: 2022/07/04 14:21:45 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
-#define MINITALK_H
+# define MINITALK_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <signal.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <signal.h>
+# include "libft/libft.h"
 
-void	show_bits(unsigned int num, int num_of_bytes);
 void	send_str_as_signals(pid_t pid, char *str);
+void	signal_catcher(int signum, siginfo_t *info, void *context);
 
 #endif
