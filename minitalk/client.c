26183ae4963b367d00bc33cecc25f6794d2e95b9
AnniYuko <yuko99@outlook.de>
2022-07-04 14:58:40 +0200

    norminette

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,30 +6,11 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 09:28:26 by akroll            #+#    #+#             */
-/*   Updated: 2022/06/29 14:01:40 by akroll           ###   ########.fr       */
+/*   Updated: 2022/07/04 14:21:35 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-// for atoi():
-#include <stdlib.h>
-
-void	show_bits(unsigned int num, int num_of_bytes)
-{
-	int				position_i;
-	unsigned long	lu;
-
-	position_i = 8 * num_of_bytes - 1;
-	lu = 1;
-	while (position_i >= 0)
-	{
-		putchar(num & (lu << position_i) ? '1' : '0');
-		if (!(position_i % 8))
-			putchar(' ');
-		position_i--;
-	}
-	putchar('\n');
-}
 
 void	send_str_as_signals(pid_t pid, char *str)
 {
@@ -59,11 +40,10 @@ int	main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		write(1, "Please enter 3 arguments:\n./client <server_pid> \"string\"\n", 58);
+		write(1, "Please enter 3 arguments:\n./client <pid> \"string\"\n", 58);
 		return (1);
 	}
-	server_pid = atoi(argv[1]);
+	server_pid = ft_atoi(argv[1]);
 	send_str_as_signals(server_pid, argv[2]);
-
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 09:28:34 by akroll            #+#    #+#             */
-/*   Updated: 2022/07/04 13:33:36 by akroll           ###   ########.fr       */
+/*   Updated: 2022/07/04 14:47:36 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	signal_catcher(int signum, siginfo_t *info, void *context)
 	unsigned int			bit;
 	static unsigned char	character;
 
+	(void)info;
+	(void)context;
 	character <<= 1;
 	bit = 0;
 	if (signum == SIGUSR1)
@@ -33,23 +35,21 @@ void	signal_catcher(int signum, siginfo_t *info, void *context)
 	}
 }
 
-int main()
+int	main(void)
 {
-	pid_t my_pid;
-	struct sigaction sigact;
+	pid_t				my_pid;
+	struct sigaction	sigact;
 
 	my_pid = getpid();
-	printf("pid: %d\n", my_pid);
+	ft_printf("pid: %d\n", my_pid);
 	sigemptyset(&sigact.sa_mask);
 	sigact.sa_sigaction = signal_catcher;
 	sigact.sa_flags = SA_SIGINFO | SA_RESTART;
-
 	if (sigaction(SIGUSR1, &sigact, NULL) == -1)
 		perror("USR1 signal");
 	if (sigaction(SIGUSR2, &sigact, NULL) == -1)
 		perror("USR2 signal");
-
 	while (1)
 		pause();
-	return 0;
+	return (0);
 }
