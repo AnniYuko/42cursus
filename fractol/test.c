7644622472ebe73901cb3beb4810dd7cc58acc5c
AnniYuko <yuko99@outlook.de>
2022-07-20 16:37:09 +0200

    nice pattern algo

--- fractol/test.c
+++ fractol/test.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 10:16:23 by akroll            #+#    #+#             */
-/*   Updated: 2022/07/20 15:48:01 by akroll           ###   ########.fr       */
+/*   Updated: 2022/07/20 16:36:19 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#define WIDTH 400
-#define HEIGHT 400
+#define WIDTH 500
+#define HEIGHT 500
 
 mlx_image_t	*g_img;
 
@@ -29,12 +29,12 @@ void	hook(void *param)
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
 	y = 0;
-	while (y < 50)
+	while (y < (int)g_img->height)
 	{
 		x = 0;
-		while (x < 50)
+		while (x < (int)g_img->width)
 		{
-			mlx_put_pixel(g_img, x, y, 0);
+			mlx_put_pixel(g_img, x, y, (x*y));
 			x++;
 		}
 		y++;
@@ -49,10 +49,10 @@ int	main(void)
 	if (!mlx)
 		exit(EXIT_FAILURE);
 	// image width and height 100
-	g_img = mlx_new_image(mlx, 100, 100);
+	g_img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	// all pixel channels to 255 (white)
 	memset(g_img->pixels, 255, g_img->width * g_img->height * sizeof(int));
-	// image position x = 50, y = 50
+	// put image at position x, y
 	mlx_image_to_window(mlx, g_img, 0, 0);
 	// add hook function to main loop
 	mlx_loop_hook(mlx, &hook, mlx);
