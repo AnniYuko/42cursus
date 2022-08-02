d2cf0331a0923af49caf65ee2147fab6689450e2
AnniYuko <yuko99@outlook.de>
2022-08-02 16:12:04 +0200

    global g_img changed to local var in main, adjusted functions

--- fractol/main.c
+++ fractol/main.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 10:16:23 by akroll            #+#    #+#             */
-/*   Updated: 2022/08/01 13:56:23 by akroll           ###   ########.fr       */
+/*   Updated: 2022/08/02 16:11:16 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,19 @@
 #define WIDTH 700
 #define HEIGHT 600
 
-mlx_image_t	*g_img;
-
 int	get_color(unsigned n, unsigned MaxIterations)
 {
-	int	blue_palette[] = {0x000B37FF, 0x001051FF, 0x001466FF, 0x011B81FF, 0x001F9BFF, 0x001B87FF, 0x0058B3FF, \
-							0x00A6D7FF, 0x30E7EDFF, 0x86FAF2FF, 0x000000FF};
-	int	color = (float)n/(float)MaxIterations * (sizeof(blue_palette)/sizeof(blue_palette[0]) - 1);
-	return (blue_palette[color]);
+	int	blue_palette[] = {0x000B37FF, 0x001051FF, 0x001466FF, 0x011B81FF, \
+		0x001F9BFF, 0x001B87FF, 0x0058B3FF, 0x00A6D7FF, 0x30E7EDFF, \
+		0x86FAF2FF, 0x000000FF};
+	return (blue_palette[(int)((float)n/(float)MaxIterations * 10)]);
+}
+
+void	detect_keys(void *param)
+{
+	mlx_t	*mlx = param;
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
 }
 
 void	hook(void *param)
@@ -36,25 +41,23 @@ void	hook(void *param)
 	double		MinIm = -1.2;
 	double		MinRe = -2.0;
 	double		MaxRe = 1.0;
-	double		MaxIm = MinIm+(MaxRe-MinRe)*g_img->height/g_img->width;
-	double		Re_factor = (MaxRe-MinRe)/g_img->width;
-	double		Im_factor = (MaxIm-MinIm)/g_img->height;
+	double		MaxIm = MinIm+(MaxRe-MinRe)*HEIGHT/WIDTH;
+	double		Re_factor = (MaxRe-MinRe)/WIDTH;
+	double		Im_factor = (MaxIm-MinIm)/HEIGHT;
 	double		Z_re;
 	double		Z_im;
 	double		Z_re2;
 	double		Z_im2;
 	unsigned	n;
 	unsigned MaxIterations = 40;
-	mlx_t	*mlx = param;
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
+	mlx_image_t	*img = param;
 
 	y = 0;
-	while (y < g_img->height)
+	while (y < HEIGHT)
 	{
 		c_im = MaxIm - y * Im_factor;
 		x = 0;
-		while (x < g_img->width)
+		while (x < WIDTH)
 		{
 			c_re = MinRe + x * Re_factor;
 
@@ -76,7 +79,7 @@ void	hook(void *param)
 				Z_re = Z_re2 - Z_im2 + c_re;
 				n++;
 			}
-				mlx_put_pixel(g_img, x, y, get_color(n, MaxIterations));
+				mlx_put_pixel(img, x, y, get_color(n, MaxIterations));
 				x++;
 		}
 		y++;
@@ -87,20 +90,22 @@ void	hook(void *param)
 int	main(void)
 {
 	mlx_t		*mlx;
+	mlx_image_t	*img;
 
-	mlx = mlx_init(WIDTH, HEIGHT, "Fractol", true);
+	mlx = mlx_init(WIDTH, HEIGHT, "fract-ol", true);
 	if (!mlx)
 		exit(EXIT_FAILURE);
 	// image width and height
-	g_img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	// all pixel channels to 255 (white)
-	memset(g_img->pixels, 255, g_img->width * g_img->height * sizeof(int));
+	memset(img->pixels, 255, img->width * img->height * sizeof(int));
 	// put image at position x, y
-	mlx_image_to_window(mlx, g_img, 0, 0);
+	mlx_image_to_window(mlx, img, 0, 0);
 	// add hook function to main loop
-	mlx_loop_hook(mlx, &hook, mlx);
+	mlx_loop_hook(mlx, &detect_keys, mlx);
+	mlx_loop_hook(mlx, &hook, img);
 	mlx_loop(mlx);
-	mlx_delete_image(mlx, g_img);
+	mlx_delete_image(mlx, img);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
