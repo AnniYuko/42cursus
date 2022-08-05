ad593cdc312e620b0966fe7a31ca31a61827f8c5
AnniYuko <yuko99@outlook.de>
2022-08-05 19:18:43 +0200

    zoom prototype

--- fractol/fractol.h
+++ fractol/fractol.h
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 16:52:50 by akroll            #+#    #+#             */
-/*   Updated: 2022/08/02 17:01:52 by akroll           ###   ########.fr       */
+/*   Updated: 2022/08/05 17:35:52 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+// #include <math.h>
 
 typedef struct s_complex {
 	double	re;
 	double	im;
+	double	re2;
+	double	im2;
 } t_complex;
 
+typedef struct s_mandelbrot {
+	double	Im_min;
+	double	Im_max;
+	double	Re_min;
+	double	Re_max;
+	t_complex	Z;
+	t_complex	c;
+	unsigned int	iter_max;
+} t_mandelbrot;
+
 typedef struct s_info {
-	mlx_t		*mlx;
-	mlx_image_t	*img;
+	mlx_t			*mlx;
+	mlx_image_t		*img;
+	t_mandelbrot	*mb;
 } t_info;
 
 #endif
--- fractol/main.c
+++ fractol/main.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 10:16:23 by akroll            #+#    #+#             */
-/*   Updated: 2022/08/04 14:03:35 by akroll           ###   ########.fr       */
+/*   Updated: 2022/08/05 18:29:10 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 #define WIDTH 700
 #define HEIGHT 600
 
-int	get_color(unsigned n, unsigned MaxIterations)
+void	initialize(t_mandelbrot *mb)
 {
-	int	blue_palette[] = {0x000B37FF, 0x001051FF, 0x001466FF, 0x011B81FF, \
-		0x001F9BFF, 0x001B87FF, 0x0058B3FF, 0x00A6D7FF, 0x30E7EDFF, \
-		0x86FAF2FF, 0x000000FF};
-	return (blue_palette[(int)((float)n/(float)MaxIterations * 10)]);
+	// f->mb->iter_max = 40;
+	mb->Re_min = -2.0;
+	mb->Re_max = 1.0;
+	mb->Im_min = -1.2;
+	mb->Im_max = mb->Im_min+(mb->Re_max - mb->Re_min)*(float)HEIGHT/(float)WIDTH;
 }
 
 void	detect_keys(void *param)
@@ -29,33 +30,61 @@ void	detect_keys(void *param)
 		mlx_close_window(mlx);
 }
 
+void zoom_hook(double xdelta, double ydelta, void* param)
+{
+	t_mandelbrot	*mb;
+	mb = param;
+	double	zoomfactor = 2;
+	double	delta_Re = mb->Re_max - mb->Re_min;
+	double	delta_Im = mb->Im_max - mb->Im_min;
+
+	(void)xdelta;
+	if (ydelta > 0)
+	{
+		puts("Zoom out");
+		mb->Re_min = mb->Re_min - (1.0/zoomfactor * delta_Re);
+		mb->Re_max = mb->Re_max + (1.0/zoomfactor * delta_Re);
+		mb->Im_min = mb->Im_min - (1.0/zoomfactor * delta_Im);
+		mb->Im_max = mb->Im_max + (1.0/zoomfactor * delta_Im);
+	}
+	else if (ydelta < 0)
+		puts("Zoom in");
+		mb->Re_min = mb->Re_min - (zoomfactor * delta_Re);
+		mb->Re_max = mb->Re_max + (zoomfactor * delta_Re);
+		mb->Im_min = mb->Im_min - (zoomfactor * delta_Im);
+		mb->Im_max = mb->Im_max + (zoomfactor * delta_Im);
+}
+
+int	get_color(unsigned n, unsigned MaxIterations)
+{
+	int	blue_palette[] = {0x000B37FF, 0x001051FF, 0x001466FF, 0x011B81FF, \
+		0x001F9BFF, 0x001B87FF, 0x0058B3FF, 0x00A6D7FF, 0x30E7EDFF, \
+		0x86FAF2FF, 0x000000FF};
+	return (blue_palette[(int)((float)n/(float)MaxIterations * 10)]);
+}
+
 void	hook(void *param)
 {
 	unsigned	x;
 	unsigned	y;
 	t_complex	c;
-	double		MinIm = -1.2;
-	double		MinRe = -2.0;
-	double		MaxRe = 1.0;
-	double		MaxIm = MinIm+(MaxRe-MinRe)*HEIGHT/WIDTH;
-	double		Re_factor = (MaxRe-MinRe)/WIDTH;
-	double		Im_factor = (MaxIm-MinIm)/HEIGHT;
 	double		Z_re;
 	double		Z_im;
 	double		Z_re2;
 	double		Z_im2;
 	unsigned	n;
 	unsigned MaxIterations = 40;
-	mlx_image_t	*img = param;
+	t_info		*f;
 
+	f = param;
 	y = 0;
 	while (y < HEIGHT)
 	{
-		c.im = MaxIm - y * Im_factor;
+		c.im = f->mb->Im_max - y * (f->mb->Im_max-f->mb->Im_min)/(float)HEIGHT;
 		x = 0;
 		while (x < WIDTH)
 		{
-			c.re = MinRe + x * Re_factor;
+			c.re = f->mb->Re_min + x * (f->mb->Re_max-f->mb->Re_min)/(float)WIDTH; // c = Re_min + (x * PixelSize), Im_max - (y * PixelSize)
 
 			// Set Z = c
 			Z_re = c.re;
@@ -75,7 +104,7 @@ void	hook(void *param)
 				Z_re = Z_re2 - Z_im2 + c.re;
 				n++;
 			}
-				mlx_put_pixel(img, x, y, get_color(n, MaxIterations));
+				mlx_put_pixel(f->img, x, y, get_color(n, MaxIterations));
 				x++;
 		}
 		y++;
@@ -90,6 +119,7 @@ int	main(void)
 	f.mlx = mlx_init(WIDTH, HEIGHT, "fract-ol", true);
 	if (!f.mlx)
 		exit(EXIT_FAILURE);
+	initialize(f.mb);
 	// image width and height
 	f.img = mlx_new_image(f.mlx, WIDTH, HEIGHT);
 	// all pixel channels to 255 (white)
@@ -98,7 +128,8 @@ int	main(void)
 	mlx_image_to_window(f.mlx, f.img, 0, 0);
 	// add hook function to main loop
 	mlx_loop_hook(f.mlx, &detect_keys, f.mlx);
-	mlx_loop_hook(f.mlx, &hook, f.img);
+	mlx_loop_hook(f.mlx, &hook, &f);
+	mlx_scroll_hook(f.mlx, &zoom_hook, f.mb);
 	mlx_loop(f.mlx);
 	mlx_delete_image(f.mlx, f.img);
 	mlx_terminate(f.mlx);
