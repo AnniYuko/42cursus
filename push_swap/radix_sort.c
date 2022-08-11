59aeda944ea77af5b7193a9d8446e1fa927888c9
AnniYuko <yuko99@outlook.de>
2022-08-11 14:14:45 +0200

    counting sort, prefix sum

--- push_swap/radix_sort.c
+++ push_swap/radix_sort.c
@@ -6,13 +6,14 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 13:42:25 by akroll            #+#    #+#             */
-/*   Updated: 2022/08/05 11:39:38 by akroll           ###   ########.fr       */
+/*   Updated: 2022/08/11 14:14:21 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define BASE 10
 
 int	print_array(int *arr, unsigned int size)
 {
@@ -24,7 +25,6 @@ int	print_array(int *arr, unsigned int size)
 		printf("element %u: %d\n", i, arr[i]);
 		i++;
 	}
-	printf("---------------------\n");
 	return (0);
 }
 
@@ -58,21 +58,61 @@ int	get_digits(int num)
 	return (count);
 }
 
-int	*radix_sort(int *arr, unsigned int size, unsigned int digits)
+void	bzero_arr(int *arr, int size)
 {
-	int	*sorted_arr;
+	int	i;
 
-	sorted_arr = calloc(size, sizeof(int));
-	(void)digits;
-	// while ()
-		// counting_sort();
-	return (sorted_arr);
+	i = 0;
+	while (i < size)
+	{
+		arr[i] = 0;
+		i++;
+	}
+}
+
+int	*radix_sort(int *input_arr, unsigned int size, unsigned int digits)
+{
+	int				*output_arr;
+	unsigned int	count_arr[BASE-1];
+	unsigned int	i;
+	unsigned int	mod;
+	unsigned int	num;
+
+	mod = 1;
+	output_arr = calloc(size, sizeof(int));
+	while (digits > 0)
+	{
+		// counting_sort
+		bzero_arr(count_arr, BASE);
+		i = 0;
+		while (i < size)
+		{
+			num = input_arr[i] / mod % 10;
+			count_arr[num] += 1;
+			i++;
+		}
+		mod *= 10;
+		digits--;
+		printf("---- counting sort \n");
+		print_array(count_arr, BASE);
+
+		// prefix sum
+		i = 1;
+		while (i < BASE)
+		{
+			count_arr[i] += count_arr[i - 1];
+			i++;
+		}
+		printf("---- prefix sum\n");
+		print_array(count_arr, BASE);
+	}
+	return (output_arr);
 }
 
 int	main()
 {
 	int	n;
-	int	arr[] = { 23, 9, 52, 467, 22, 142, 90, 1 };
+	int	arr[] = { 23, 9, 52, 90, 1 };
 	int	*sorted_arr;
 	int	digits;
 	int	max;
@@ -84,6 +124,8 @@ int	main()
 
 	printf("biggest number: %d\n", max);
 	printf("digits: %d\n\n", digits);
+	printf("------- input -----------\n");
 	print_array(arr, n);
+	printf("------- output -----------\n");
 	print_array(sorted_arr, n);
 }
