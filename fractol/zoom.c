ab0329ac63f3adad8a13b8422470007b60c0e533
AnniYuko <yuko99@outlook.de>
2022-08-30 17:00:52 +0200

    zoom in: make smaller window, only draw what's inside the smaller window. Stretching still missing

--- fractol/zoom.c
+++ fractol/zoom.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 12:27:20 by akroll            #+#    #+#             */
-/*   Updated: 2022/08/15 14:38:28 by akroll           ###   ########.fr       */
+/*   Updated: 2022/08/30 16:58:50 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,19 @@
 #include <unistd.h>
 #define WIDTH 500
 #define HEIGHT 500
+#define BLACK 0
 #define BPP sizeof(int)
 
+typedef struct screen_coord {
+	int x_start;
+	int y_start;
+	int x_end;
+	int y_end;
+	int scale;
+	int offset;
+} screen_coord;
+
+screen_coord	screen;
 
 void	detect_keys(void *param)
 {
@@ -28,8 +39,10 @@ void	detect_keys(void *param)
 
 void zoom_hook(double xdelta, double ydelta, void* param)
 {
-	(void)param;
+	int zoom = 10;
 	(void)xdelta;
+	(void)param;
+
 	if (ydelta > 0)
 	{
 		puts("Zoom out");
@@ -37,23 +50,31 @@ void zoom_hook(double xdelta, double ydelta, void* param)
 	else
 	{
 		puts("Zoom in");
+		screen.x_start += zoom;
+		screen.x_end -= zoom;
+
+		// screen.scale = (screen.x_end - screen.x_start) / (screen.y_end - screen.y_start);
+		// screen.offset = 0;
+		screen.y_start += zoom;
+		screen.y_end -= zoom;
 	}
 }
 
-void	draw_line_horizontal(int x_start, int y_start, int size, mlx_image_t *img)
+void	draw_square(int x_start, int y_start, int size, mlx_image_t *img)
 {
 	int	x;
 	int	y;
 
-	y = y_start;
-	while (y < y_start + size)
+	y = screen.y_start;
+	while (y < screen.y_end)
 	{
-		x = 0;
-		while (x < WIDTH)
+		x = screen.x_start;
+		while (x < screen.x_end)
 		{
-			if (x > x_start)
+			if (y >= y_start && y < y_start + size
+				&& x >= x_start && x < x_start + size)
 			{
-				mlx_put_pixel(img, x, y, 0);
+				mlx_put_pixel(img, x, y, BLACK);
 			}
 			x++;
 		}
@@ -64,17 +85,13 @@ void	draw_line_horizontal(int x_start, int y_start, int size, mlx_image_t *img)
 void	hook(void *param)
 {
 	mlx_image_t	*img;
-	int			y;
-	// float	x_offs = -2;
-	// float	y_offs = -2;
 
 	img = param;
-	y = 0;
-	while (y < HEIGHT)
-	{
-		draw_line_horizontal(0, y, 3, img);
-		y += 10;
-	}
+	memset(img->pixels, 255, img->width * img->height * BPP);
+	draw_square(0, 0, 30, img);
+	draw_square(100, 50, 10, img);
+	draw_square(200, 200, 35, img);
+	draw_square(400, 400, 40, img);
 }
 
 int main()
@@ -82,6 +99,12 @@ int main()
 	mlx_t		*mlx;
 	mlx_image_t	*img;
 
+	screen.x_start = 0;
+	screen.y_start = 0;
+	screen.x_end = 500;
+	screen.y_end = 500;
+	// screen.scale = 1;
+	// screen.offset = 0;
 	mlx = mlx_init(WIDTH, HEIGHT, "fract-ol", true);
 	if (!mlx)
 		exit(EXIT_FAILURE);
@@ -93,8 +116,8 @@ int main()
 	memset(img->pixels, 255, img->width * img->height * BPP);
 	// add hook function to main loop
 	mlx_loop_hook(mlx, &detect_keys, mlx);
-	mlx_scroll_hook(mlx, &zoom_hook, NULL);
 	mlx_loop_hook(mlx, &hook, img);
+	mlx_scroll_hook(mlx, &zoom_hook, NULL);
 	mlx_loop(mlx);
 	mlx_delete_image(mlx, img);
 	mlx_terminate(mlx);
