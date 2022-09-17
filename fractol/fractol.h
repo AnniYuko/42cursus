ad40a5a805aca17fd2731e5e4cd9687158b4a9d0
AnniYuko <yuko99@outlook.de>
2022-09-17 17:19:00 +0200

    add julia

--- fractol/fractol.h
+++ fractol/fractol.h
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 16:52:50 by akroll            #+#    #+#             */
-/*   Updated: 2022/08/05 17:35:52 by akroll           ###   ########.fr       */
+/*   Updated: 2022/09/17 16:19:39 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 // #include <math.h>
 
 typedef struct s_complex {
 	double	re;
 	double	im;
-	double	re2;
-	double	im2;
 } t_complex;
 
-typedef struct s_mandelbrot {
-	double	Im_min;
-	double	Im_max;
-	double	Re_min;
-	double	Re_max;
-	t_complex	Z;
-	t_complex	c;
+typedef struct s_fractal {
+	double			Im_min;
+	double			Im_max;
+	double			Re_min;
+	double			Re_max;
 	unsigned int	iter_max;
-} t_mandelbrot;
+	bool			julia;
+	int				K;
+	bool			mandel;
+	t_complex		Z;
+	t_complex		c;
+} t_fractal;
 
 typedef struct s_info {
-	mlx_t			*mlx;
-	mlx_image_t		*img;
-	t_mandelbrot	*mb;
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+	t_fractal	fract;
 } t_info;
 
 #endif
