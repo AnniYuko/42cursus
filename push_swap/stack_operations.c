4257dc93fd686b01c9cdb43b9c8e236702b0bc3d
AnniYuko <yuko99@outlook.de>
2022-09-24 17:28:06 +0200

    list reverse rotate

--- push_swap/stack_operations.c
+++ push_swap/stack_operations.c
@@ -6,7 +6,7 @@
 /*   By: akroll <akroll@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 13:17:05 by akroll            #+#    #+#             */
-/*   Updated: 2022/09/24 16:26:58 by akroll           ###   ########.fr       */
+/*   Updated: 2022/09/24 16:53:43 by akroll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,12 +44,22 @@ t_list	*ft_lst_rotate(t_list *top)
 	ft_lstlast(top)->next = top;
 	new_top = top->next;
 	top->next = NULL;
-
 	return (new_top);
 }
 
-// // shift down all elements, last elem becomes first
-// t_list	*ft_lst_reverse_rotate(t_list *top)
-// {
+// shift down all elements, last elem becomes first
+t_list	*ft_lst_reverse_rotate(t_list *top)
+{
+	t_list	*penultimate;
+	t_list	*new_top;
 
-// }
+	penultimate = top;
+	while (penultimate->next->next != NULL)
+	{
+		penultimate = penultimate->next;
+	}
+	new_top = penultimate->next;
+	new_top->next = top;
+	penultimate->next = NULL;
+	return (new_top);
+}
